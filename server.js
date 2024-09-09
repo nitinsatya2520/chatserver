9922bdeb6a9bb6fbe7e58cd7b10f023aeacad0b7
nitinsatya <nitinsatya656gmail.com>
2024-09-09 19:32:43 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST']
+  }
+});
+
+app.use(cors());
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  // Listen for message events and broadcast them with the username
+  socket.on('message', (payload) => {
+    io.emit('message', payload); // Broadcast to all users
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+server.listen(4000, () => {
+  console.log('Server running on port 4000');
+});
