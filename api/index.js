9099e388defeb56e5e2a0f85485466341053feda
nitinsatya <nitinsatya656gmail.com>
2024-09-10 09:00:13 +0530

    Fix start script to point to server.js

--- api/index.js
+++ api/index.js
@@ -1,4 +1,42 @@
-export default function handler(req, res) {
-    res.status(200).json({ message: 'Hello from Vercel!' });
-  }
-  
+const { Server } = require('socket.io');
+const cors = require('cors');
+const express = require('express');
+
+const app = express();
+
+const allowedOrigins = ['http://localhost:3000', 'https://kns-chat-app.vercel.app'];
+
+app.use(cors({
+  origin: allowedOrigins,
+  methods: ['GET', 'POST'],
+  credentials: true,
+}));
+
+const server = require('http').createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: allowedOrigins,
+    methods: ['GET', 'POST'],
+    credentials: true,
+  },
+  transports: ['websocket'], // Using WebSocket only
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('message', (payload) => {
+    io.emit('message', payload);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+});
+
+module.exports = (req, res) => {
+  server.listen(4000, () => {
+    res.send(`Server running on port 4000`);
+  });
+};
